b451cb9e99efda6eb3aad2660cb66f05259be1de
Andres Breton <bretonics@users.noreply.github.com>
2016-04-19 23:31:23 -0400

    fixed initial commit

--- Workflows/seqAnalysis.pl
+++ Workflows/seqAnalysis.pl
@@ -13,10 +13,11 @@ use Getopt::Long; use Pod::Usage;
 #
 # =============================================================================
 
-#-------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
 # COMMAND LINE
-my $SRA = "";
-my $ACCESSION = "";
+my $SRA;
+my $ACCESSION;
 my $usage = "\n\n$0 [options]\n
 Options:
     -sra          SRA ID
@@ -30,57 +31,35 @@ GetOptions(
     'accession:s'   =>\$ACCESSION,
     help            =>sub{pod2usage($usage);}
 )or pod2usage(2);
-#-------------------------------------------------------------------------
+
+argChecks(); #check CL arguments were passed
+#-------------------------------------------------------------------------------
 # VARIABLES
-my $index = "MG1655"; #reference index name
+my $index   = "MG1655"; #reference index name
 my $sraRepo = "~/ncbi/public/sra"; #default SRA directory
 my $outDir 	= setOutputDir('analysis'); #create analysis directory
-
-# Hash of Hashes of software with respective commands used in the workflow.
-# This simplifies both visual and future modifications you'd like to
-# make for each command. One place, one change.
-my %COMMANDS = (
-    edirect             => "esearch -db nucleotide -query \"$ACCESSION [ACCN]\" | efetch -format fasta > $ACCESSION.fasta",
-    prefetch            => "prefetch -c $SRA",
-    "fastq-dump"        => "fastq-dump --split-files $SRA ; fastq-dump --fasta $SRA",
-    "bowtie2-build"     => "bowtie2-build -o 3 $ACCESSION.fasta $index",
-    bowtie2             => "bowtie2 -x $index -1 $SRA\_1.fastq -2 $SRA\_2.fastq -S $SRA.sam",
-    samtools            => {
-                            view        => "samtools view -b $SRA.sam > $SRA.bam",
-                            sort        => "samtools sort $SRA.bam $SRA.sorted", #server has different version, thus no < -o > flag
-                            index       => "samtools index $SRA.sorted.bam",
-                            tview       => "samtools tview $SRA.sorted.bam",
-                            consensus   => "samtools mpileup -uf $ACCESSION.fasta $SRA.sorted.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus.fastq",
-                            variants    => "samtools mpileup -uf $ACCESSION.fasta $SRA.sorted.bam | bcftools call -mv -Oz > variants.vcf.gz",
-                            },
-    seqtk               => "seqtk seq -A consensus.fastq > consensus.fasta",
-    nucmer              => "nucmer -maxmatch -c 100 -p nucmer $ACCESSION.fasta consensus.fasta",
-    "show-coords"       => "show-coords -r -c -l nucmer.delta > nucmer.coords",
-    mummerplot          => "mummerplot --png --color -p nucmer nucmer.delta",
-);
-#-------------------------------------------------------------------------
+my $commands = getCommands($SRA, $ACCESSION);
+#-------------------------------------------------------------------------------
 # CALLS
-argChecks(); #check CL arguments were passed
-executeCommand("edirect"); #edirect
-executeCommand("prefetch"); #prefetch
-executeCommand("fastq-dump"); #fastq-dump
-executeCommand("bowtie2-build"); #bowtie2-build
-executeCommand("bowtie2"); #bowtie2
-executeCommand("samtools", "view"); #samtools view
-executeCommand("samtools", "sort"); #samtools sort
-executeCommand("samtools", "index"); #samtools index
+executeCommand("edirect", 		$commands); # edirect
+executeCommand("prefetch", 		$commands); # prefetch
+executeCommand("fastq-dump", 	$commands); # fastq-dump
+executeCommand("bowtie2-build", $commands); # bowtie2-build
+executeCommand("bowtie2", 		$commands); # bowtie2
+executeCommand("samtools", 		$commands, "view" ); # samtools view
+executeCommand("samtools", 		$commands, "sort");  # samtools sort
+executeCommand("samtools", 		$commands, "index"); # samtools index
 # samtools 'tview' commented b/c you don't want to interupt your analysis
 # with a screen...execute command above once done to see alignment
-# executeCommand("samtools", "tview"); #samtools tview
-executeCommand("samtools", "consensus"); #samtools consensus
-executeCommand("seqtk"); #seqtk
-executeCommand("nucmer"); #nucmer
-executeCommand("show-coords"); #show-coords
-executeCommand("mummerplot"); #mummerplot
-executeCommand("samtools", "variants"); #samtools variants
-#-------------------------------------------------------------------------
+#executeCommand("samtools", 		$commands, "tview"); # samtools tview
+executeCommand("samtools", 		$commands, "consensus"); # samtools consensus
+executeCommand("seqtk", 		$commands); # seqtk
+executeCommand("nucmer", 		$commands); # nucmer
+executeCommand("show-coords", 	$commands); # show-coords
+executeCommand("mummerplot", 	$commands); # mummerplot
+executeCommand("samtools", 		$commands, "variants"); # samtools variants
+#-------------------------------------------------------------------------------
 # SUBS
-
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # $input = argChecks();
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -99,27 +78,68 @@ sub argChecks {
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# $input = executeCommand($call, $option);
+# $input = ($SRA, $ACCESSION);
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# This function takes arguments 1 or 2 arguments; the call to
-# the program to be executed and an optional option for commands
-# requiring a second flag (such as samtools)
+# This function takes two (2) arguments, the SRA and accession.
+# Creatse and anonymous hash with commands as keys, values as
+# command to execute, returning a reference to the hash of
+# software. This simplifies both visual and future modifications
+# you'd like to make for each command. One place, one change.
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# $return = ($commands);
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+sub getCommands {
+    my ($sra, $accession) = @_;
+
+    my $commands = {
+        "edirect"           => "esearch -db nucleotide -query \"$ACCESSION [ACCN]\" | efetch -format fasta > $ACCESSION.fasta",
+        "prefetch"          => "prefetch -c $SRA",
+        "fastq-dump"        => "fastq-dump --split-files $SRA ; fastq-dump --fasta $SRA",
+        "bowtie2-build"     => "bowtie2-build -o 3 $ACCESSION.fasta $index",
+        "bowtie2"           => "bowtie2 -x $index -1 $SRA\_1.fastq -2 $SRA\_2.fastq -S $SRA.sam",
+        # Hash of hashes for samtools since it has multipe option calls
+        "samtools"          => {
+                                view        => "samtools view -b $SRA.sam > $SRA.bam",
+                                sort        => "samtools sort $SRA.bam $SRA.sorted", #server has different version, thus no < -o > flag
+                                index       => "samtools index $SRA.sorted.bam",
+                                tview       => "samtools tview $SRA.sorted.bam",
+                                consensus   => "samtools mpileup -uf $ACCESSION.fasta $SRA.sorted.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus.fastq",
+                                variants    => "samtools mpileup -uf $ACCESSION.fasta $SRA.sorted.bam | bcftools call -mv -Oz > variants.vcf.gz",
+                                },
+        "seqtk"             => "seqtk seq -A consensus.fastq > consensus.fasta",
+        "nucmer"            => "nucmer -maxmatch -c 100 -p nucmer $ACCESSION.fasta consensus.fasta",
+        "show-coords"       => "show-coords -r -c -l nucmer.delta > nucmer.coords",
+        "mummerplot"        => "mummerplot --png --color -p nucmer nucmer.delta",
+    };
+    return ($commands);
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# $input = executeCommand($call, $commands, $option);
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# This function takes arguments 2 or 3 arguments; the call to
+# the program to be executed, the hash of commands, and an
+# optional 'option' for commands requiring a second flag
+# (such as samtools).
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # $output = Executes command and reports status
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 sub executeCommand {
-    my ($call, $option) = @_;
+    my ($call, $command, $option) = @_;
+    my $exec;
+
     if ($option) {
-        say "Executing $call...\n$COMMANDS{$call}{$option}\n";
-        my $result = `$COMMANDS{$call}{$option}`;
-        failedEx($COMMANDS{$call}{$option}) if ($? != 0);
-        say "Done.\n\n";
+        $exec = $commands->{$call}{$option};
     } else {
-        say "Executing $call...\n$COMMANDS{$call}\n";
-        my $result = `$COMMANDS{$call}`;
-        failedEx($COMMANDS{$call}) if ($? != 0);
-        say "Done.\n\n";
+        $exec = $commands->{$call};
     }
+
+    say "Executing $call...\n\t$exec\n";
+    my $result = `$exec`;
+    # $?  Child error. Status code of most recent system call or pipe.
+    failedEx($exec) if ($? != 0);
+
+    say "Done.\n\n";
     return;
 }
 
@@ -134,17 +154,17 @@ sub executeCommand {
 sub failedEx {
     my ($command) = @_;
     die	"WARNING: Something seems to have gone wrong!\n",
-    	"Failed to execute command " , $command , "\n\n",
-    	"Please check installed software version and/or permissions", $!;
+    	"Failed to execute '" , $command , "'\n",
+    	"Please check installed software version and/or permissions\n\n", $!;
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# $input = VOID context
+# $input = ($ourDirName)
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # This function takes no arguments, creates an output directory
 # if non-existent for results, and moves into it
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# $output = 'analysis' output directory
+# $output = '$ourDirName' output directory
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 sub setOutputDir {
     my ($outDir) =  @_;
